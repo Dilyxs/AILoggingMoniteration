a93e14251b304029963b35c8635cc46fabe42e52
dilyxs <selvan@adsayan.com>
2025-09-07 23:40:52 +0000

    added detection of Trade Log --Problem now of added manual print statement that ruin the mood

--- Initial.py
+++ Initial.py
@@ -1,4 +1,6 @@
 from PostGresConn import  PostgresSQL
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from huggingface_hub import whoami
 
 class Test:
     def __init__(self):
@@ -7,6 +9,17 @@ class Test:
         self.data = self.FetchLoggers();#this return a list with each row of db as a Map;
         self.AddLastLoggedLines()
         self.AddLinesToView()
+        self.model = self.ReturnModel()
+        self.DetectIfErrorLarge()
+
+
+    def ReturnModel(self,model_name ="byviz/bylastic_classification_logs"):
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        
+        classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+        return classifier
+
 
     def FetchLoggers(self):
         return self.db.FetchAllData("loggerpath")
@@ -18,7 +31,9 @@ class Test:
     
     def AddLastLoggedLines(self):
         for data in self.data:
-            data['lastloggedline'] = self.FindLastLoggedLine(data['id'])['lastline'] if self.FindLastLoggedLine(data['id']) else None
+            localdata = self.FindLastLoggedLine(data['id'])
+            localdata = 0 if not localdata else localdata['lastline']
+            data['lastloggedline'] = localdata
 
     def LoggedLinesToInspect(self,filepath,lastloggedline):
         with open(filepath, "r") as f:
@@ -35,6 +50,24 @@ class Test:
             
             data['LinesToView'] = self.LoggedLinesToInspect(data['loggerpath'], data['lastloggedline'])[0]
 
+    def DetectIfError(self,filepath, model, StartLine = 0, Endline = None):
+        ErrorDetected = False
+        with open(filepath, 'r') as f:
+            lines  = f.readlines()
+            
+        TrainingLines = lines[StartLine:] if not Endline else lines[StartLine:Endline]
+        for line in TrainingLines:
+            result = (model(line))[0]
+            if result['label'] == "ERROR" and result['score'] >=0.7:
+                ErrorDetected=True
+                break
+
+        return ErrorDetected
+    def DetectIfErrorLarge(self):
+        for data in self.data:
+            data['ErrorPresent'] = self.DetectIfError(data['loggerpath'], self.model, data['lastloggedline'], None)
 
 a = Test()
-print(a.data)
+for i in a.data:
+    print(i['loggerpath'])
+    print(i['ErrorPresent'])
