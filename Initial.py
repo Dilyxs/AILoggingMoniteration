1046a846d19b14485a641a026b06ad65ad7da54d
dilyxs <selvan@adsayan.com>
2025-09-07 11:00:59 -0400

    added for details for map, including LastLoggedLiles, NewLinesTosee and LewLastLoggedLines, now, need to add in AI model

--- Initial.py
+++ Initial.py
@@ -1,1 +1,40 @@
+from PostGresConn import  PostgresSQL
 
+class Test:
+    def __init__(self):
+        self.db = PostgresSQL()
+        self.Loggers = []
+        self.data = self.FetchLoggers();#this return a list with each row of db as a Map;
+        self.AddLastLoggedLines()
+        self.AddLinesToView()
+
+    def FetchLoggers(self):
+        return self.db.FetchAllData("loggerpath")
+    
+    def FindLastLoggedLine(self, LoggerID):
+        data = self.db.FetchSpecificData("lastloggedlines",condition="loggerid = %s", params=[LoggerID])
+
+        return None if not data else data
+    
+    def AddLastLoggedLines(self):
+        for data in self.data:
+            data['lastloggedline'] = self.FindLastLoggedLine(data['id'])['lastline'] if self.FindLastLoggedLine(data['id']) else None
+
+    def LoggedLinesToInspect(self,filepath,lastloggedline):
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+
+        ValidLines = lines[:] if not lastloggedline else lines[lastloggedline+1:]
+        NewLastLoggedLines = len(lines)-1
+
+        return ValidLines, NewLastLoggedLines
+    
+    def AddLinesToView(self):
+        for data in self.data:
+            data['NewLastLoggedLines'] = self.LoggedLinesToInspect(data['loggerpath'], data['lastloggedline'])[1]
+            
+            data['LinesToView'] = self.LoggedLinesToInspect(data['loggerpath'], data['lastloggedline'])[0]
+
+
+a = Test()
+print(a.data)
